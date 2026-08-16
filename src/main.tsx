fd74478d796894c9cc9037304f62b0adb4ce3f2d
David Milla Torres <desorden.help@gmail.com>
2026-08-17 01:01:06 +0200

    Mount progressive 3D app shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import AppShell from './AppShell';
 import './index.css';
+import './a11y.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppShell />
   </StrictMode>,
 );
