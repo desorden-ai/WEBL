0120251588e20c8903a1231169bf623e359ff0f4
David Milla Torres <desorden.help@gmail.com>
2026-08-17 00:30:29 +0200

    Remove inactive legacy 3D runtime sources

--- src/components/Experience.tsx
+++
@@ -1,9 +0,0 @@
-import { CinematicScroll } from './CinematicScroll';
-
-export default function Experience() {
-  return (
-    <main className="w-full min-h-screen bg-black text-neutral-100 font-sans">
-      <CinematicScroll />
-    </main>
-  );
-}
