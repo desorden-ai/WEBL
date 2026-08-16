19a0e328c766caef2e6c57d83c54ba6ed758b53d
David Milla Torres <desorden.help@gmail.com>
2026-08-17 00:24:20 +0200

    Wire optimized mansion experience entrypoint

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import Experience from './components/Experience';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Experience />
+    <App />
   </StrictMode>,
 );
